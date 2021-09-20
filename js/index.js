79ca8afa55e70ffd830703725d68d28fef096482
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2021-09-20 19:11:10 -0400

    skeleton express setup

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
-const http = require('http')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 })
