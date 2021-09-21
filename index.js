8701a1ee8e8a2388922bd22f62b57d9d70aeadef
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2021-09-21 08:36:04 -0400

    added more files and organized

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname + '/public')))
+app.use(express.static(path.join(__dirname + '/public/')))
 
 app.get('/', (req,res) => {
+  //Do AUTH checks here
   res.redirect(req.baseUrl + '/public/' + req.query);
 })
 
