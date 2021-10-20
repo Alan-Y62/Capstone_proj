50b4f257b0573df20ae7c8521b589d1353d2ba22
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2021-10-20 09:46:15 -0400

    admin repair functionality and responsive css

--- routes/main.js
+++ routes/main.js
@@ -7,37 +7,24 @@ const { subMail , sendMessage , sendUpdate} = require('../email/sendEmail')
 const { checkAuthenticated,checkRoles } = require('../public/scripts/auth')
 const Tenants = require('../model/tenants')
 const Repair = require('../model/repairModel')
+const { addTwoWeeks, generateRepairs } = require('../public/scripts/miscFuncs')
 
-const tenant1 = new Tenants({
-    ID: '002',
-    name: 'Bobby Flays',
-    address: '123 Main Street',
-    APT: '2A',
-    ARR: [1,2,3,4,5]
-})
-
-let date1 = new Date()
-date1.setFullYear(1999,11,3)
-let date2 = new Date()
-date2.setFullYear(1999,11,4)
-let date3 = new Date()
-date3.setFullYear(1999,11,5)
-
-function addTwoWeeks(date) {
-    let d = new Date(date).getTime()
-    let adds = 60*60 * 1000 * 24 * 1
-    let newDate = new Date(d + adds)
-    return newDate
-}
-
-router.get('/',checkAuthenticated, checkRoles, async (req,res) => {
+router.get('/',/*checkAuthenticated,*/ /*checkRoles,*/ async (req,res) => {
     const announce = await Announce.find()
     //console.log(req.session)                   //retrieves users name can be used to retrieve users' email
     res.render('./admin/admin_announce', {stuff:announce, user:req.user})
 })
 
 router.get('/prepage', (req,res) => {
-    res.render('./main')
+    let name = 'stranger'
+    res.render('./main', {name: name})
+})
+
+//route for generating data
+router.get('/generate', async (req,res) => {
+    const re = await generateRepairs()
+    console.log('generated data')
+    res.send('<h1>generated data</h1>')
 })
 
 router.get('/user', async (req,res) => {
@@ -48,6 +35,11 @@ router.get('/user', async (req,res) => {
 router.get('/tenant', async (req,res) => {
     const tenant = await Tenants.find()
     res.render('./admin/tenants', {list: tenant})
+})
+
+router.get('/repairh', async (req,res) => {
+    const abc123 = await Repair.find()
+    res.render('./admin/repairHA', {completed: abc123})
 })
 
 router.get('/repair_admin', async (req,res) => {
@@ -62,7 +54,7 @@ router.get('/repair_admin/p/:id/:date', async (req,res) => {
 })
 
 router.get('/repair_admin/d/:id/', async (req,res) => {
-    await Repair.findByIdAndRemove(req.params.id)
+    await Repair.findByIdAndUpdate(req.params.id, {status: 'completed'})
     res.redirect('/m/repair_admin')
 })
 
@@ -71,16 +63,16 @@ router.post('/tenant', (req,res) => {
     res.send('<h1>Successfully Deleted<h1>')
 })
 
-router.get('/new', checkAuthenticated, (req,res) => {
+router.get('/new', /*checkAuthenticated,*/ (req,res) => {
     res.render('./admin/newann')
 })
 
-router.get('/edit/:id', checkAuthenticated, async (req, res) => {
+router.get('/edit/:id', /*checkAuthenticated,*/ async (req, res) => {
     const announce = await Announce.findById(req.params.id)
     res.render('./admin/editann', { stuff: announce})
   })
 
-router.post('/new', checkAuthenticated, async (req,res) =>{
+router.post('/new', /*checkAuthenticated,*/ async (req,res) =>{
     const{title, body} = req.body
     console.log(req.user.email);
     const n_message = new Announce({
@@ -92,17 +84,17 @@ router.post('/new', checkAuthenticated, async (req,res) =>{
     res.redirect('/m');
 })
 
-router.post('/edit/:id', checkAuthenticated, async(req, res) => {
+router.post('/edit/:id', /*checkAuthenticated,*/ async(req, res) => {
     const{title, body} = req.body
     await Announce.findByIdAndUpdate(req.params.id, {title,body});
     res.redirect('/m');
 })
 
-router.get('/settings', checkAuthenticated, (req,res) => {
+router.get('/settings', /*checkAuthenticated,*/ (req,res) => {
     res.render('settings',{user:req.user})
 })
 
-router.post('/settings', checkAuthenticated, (req,res) => {
+router.post('/settings', /*checkAuthenticated,*/ (req,res) => {
     var buttonValue = req.body.button;
     const{ name,email,subject,message,subname,subemail } = req.body;
     console.log(req.body)
