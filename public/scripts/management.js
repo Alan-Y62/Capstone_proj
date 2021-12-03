594d102bfcd29fdd9974255a0a4914aa26db2576
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2021-12-03 14:47:34 -0500

    css changes & rewrote post method on management page

--- public/scripts/management.js
+++ public/scripts/management.js
@@ -1,9 +1,49 @@
+const lo = document.getElementById('location').value
+
 function postDelete(path,params,method='post'){
     const form = document.createElement('form');
     console.log(path)
     form.method = method;
     form.action = path;
     console.log(params)
+    for(const keys in params){
+        if(params.hasOwnProperty(keys)){
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = keys;
+            input.value = params[keys];
+            form.appendChild(input);
+        }
+    }
+    document.body.appendChild(form);
+    form.submit();
+ }
+
+ function pendAccept(params,method="post"){
+    const chooseApt = document.getElementById(String(params.ident)).value
+    console.log(chooseApt)
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = `/admin/${lo}/manage/useraccept`;
+    params.chooseApt = chooseApt
+    for(const keys in params){
+        if(params.hasOwnProperty(keys)){
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = keys;
+            input.value = params[keys];
+            form.appendChild(input);
+        }
+    }
+    document.body.appendChild(form);
+    form.submit();
+ }
+
+ function pendDeny(params,method="post"){
+    const chooseApt = document.getElementById(String(params.ident)).value
+    const form = document.createElement('form');
+    form.method = method;
+    form.action =  `/admin/${lo}/manage/userdeny`;
     for(const keys in params){
         if(params.hasOwnProperty(keys)){
             const input = document.createElement('input');
